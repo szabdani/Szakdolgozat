0b14307715bc724d2e407e70d5b815868eb6fd6a
szabdani <sz.dani.sd@gmail.com>
2024-10-18 19:42:27 +0200

    Database connected, early CRUD implemented

--- MauiBlazorWeb/MauiBlazorWeb.Maui/obj/Debug/net8.0-android/MauiBlazorWeb.Maui.AssemblyInfo.cs
+++ MauiBlazorWeb/MauiBlazorWeb.Maui/obj/Debug/net8.0-android/MauiBlazorWeb.Maui.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiBlazorWeb.Maui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6c0d1a0f124e13c4c205673f624116db85b755d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3e39bd38da0159e9dfd00dce5e5de972fb619159")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiBlazorWeb.Maui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiBlazorWeb.Maui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
