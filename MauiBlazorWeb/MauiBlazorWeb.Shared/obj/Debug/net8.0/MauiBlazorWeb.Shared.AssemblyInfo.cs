271bf44debef6315f1607164f08961aa68d6b5c8
szabdani <sz.dani.sd@gmail.com>
2024-11-03 11:19:40 +0100

    New div for body and isLoggedIn check for pages.

--- MauiBlazorWeb/MauiBlazorWeb.Shared/obj/Debug/net8.0/MauiBlazorWeb.Shared.AssemblyInfo.cs
+++ MauiBlazorWeb/MauiBlazorWeb.Shared/obj/Debug/net8.0/MauiBlazorWeb.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiBlazorWeb.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a55938a9c5adcfbc18ba2ce9ebaf035598fcee08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609c68cc1df2ad4bfb34b0dc9cfae35e659b370e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiBlazorWeb.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiBlazorWeb.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
