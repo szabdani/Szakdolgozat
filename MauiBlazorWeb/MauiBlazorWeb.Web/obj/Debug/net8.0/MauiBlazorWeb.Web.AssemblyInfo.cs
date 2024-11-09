b1301fe35cb759a2e825ab609202bb556a35a21f
szabdani <sz.dani.sd@gmail.com>
2024-11-09 23:51:09 +0100

    HabitStat done, LoadingScreen in use.

--- MauiBlazorWeb/MauiBlazorWeb.Web/obj/Debug/net8.0/MauiBlazorWeb.Web.AssemblyInfo.cs
+++ MauiBlazorWeb/MauiBlazorWeb.Web/obj/Debug/net8.0/MauiBlazorWeb.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiBlazorWeb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b716a86daa2c40a820354bf252a42013e0cbf31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c3f3d1d3a3678d832a0832e856fdaaa45208d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiBlazorWeb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiBlazorWeb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
