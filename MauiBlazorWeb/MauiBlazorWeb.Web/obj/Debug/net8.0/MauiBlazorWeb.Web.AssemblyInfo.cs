af8223ff9e9212e28d5b1a0ba64a5cd13fcf11a9
szabdani <sz.dani.sd@gmail.com>
2024-11-24 13:58:35 +0100

    Test page for leave confirmation

--- MauiBlazorWeb/MauiBlazorWeb.Web/obj/Debug/net8.0/MauiBlazorWeb.Web.AssemblyInfo.cs
+++ MauiBlazorWeb/MauiBlazorWeb.Web/obj/Debug/net8.0/MauiBlazorWeb.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiBlazorWeb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b6e12e0994760f074e2fb047579019c982994a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+930f9ff7591c93fa79c569da1eefa2cb80353c01")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiBlazorWeb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiBlazorWeb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
